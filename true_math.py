a677e86d1d5ca4e520020ac3d4dd0d1c7008c73d
Liliya-mama6 <vozdvizhenka@mail.ru>
2024-10-26 06:44:49 +0500

    Update true_math.p

--- true_math.py
+++ true_math.py
@@ -1,36 +1,22 @@
-from threading import Thread
-from time import sleep
+import urban
+import unittest
 
-knights = []
-
-
-class Knight(Thread):
-    def __init__(self, name, power):
-        super().__init__()
-        self.name = name
-        self.power = power
-        self.pow = 100
-
-    def run(self):
-        if self.pow == 100:
-            print(f'{self.name}, на нас напали')
-        i = 1
-        while self.pow > 0:
-            self.pow -= self.power
-            if self.pow <= 0:
-                self.pow = 0
-                print(f'{self.name}, сражается {i} день(дня)..., осталось {self.pow} воинов.'+'\n', end='')
-            else:
-                print(f'{self.name}, сражается {i} день(дня)..., осталось {self.pow} воинов.'+'\n', end='')
-                i += 1
-                sleep(1)
-        print(f'{self.name} одержал победу спустя {i} дней(дня)!'+'\n', end='')
-
-
-first_knight = Knight('Sir Lancelot', 10)
-second_knight = Knight("Sir Galahad", 20)
-first_knight.start()
-second_knight.start()
-first_knight.join()
-second_knight.join()
-print('Все битвы закончились!')
+class RunnerTest(unittest.TestCase):
+    def test_walk(self):
+        a=urban.Runner('turtle')
+        for i in range(10):
+            a.walk()
+        self.assertEqual(a.distance, 50)
+    def test_run(self):
+        a=urban.Runner('frog')
+        for i in range(10):
+            a.run()
+        self.assertEqual(a.distance, 100)
+    def test_challenge(self):
+        a=urban.Runner('pomidurov')
+        b=urban.Runner('oguchok')
+        for i in range(10):
+            a.run()
+            b.walk()
+        self.assertNotEqual(a.distance, b.distance)
+    
