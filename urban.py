8369bb8388ed696c46c8ee09396aa71f3a4e86e7
Liliya-mama6 <vozdvizhenka@mail.ru>
2024-11-09 07:37:58 +0500

    дб - varvara

--- urban.py
+++ urban.py
@@ -1,13 +1,30 @@
-class Runner:
-    def __init__(self, name):
-        self.name = name
-        self.distance = 0
+import sqlite3
+contact = sqlite3.connect('dt.db')
+cursor = contact.cursor()
 
-    def run(self):
-        self.distance += 10
 
-    def walk(self):
-        self.distance += 5
+def initiate_db():
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS Products(
+    id INTEGER PRIMARY KEY, 
+    title TEXT NOT NULL,
+    description TEXT, 
+    price INT NOT NULL
+    )
+    ''')
 
-    def __str__(self):
-        return self.name
+
+initiate_db()
+'''for i in range(1, 5):
+    cursor.execute('INSERT INTO Products(title, description, price) VALUES (?, ?, ?)',
+                   (f'Продукт{i}', f"Описание{i}", f"{i * 100}"))
+'''
+
+s=[]
+def get_all_products():
+    cursor.execute('SELECT * FROM Products')
+    a = cursor.fetchall()
+    s=[]
+    for i in a:
+        s.append(list(i))
+    return s
